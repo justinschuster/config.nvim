b4bf930ae869cd272b692441f12ae29f4f26c767
justinschuster <justin@justins-Mac-mini.local>
2025-02-28 03:37:30 -0500

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,37 @@
+# set <space> as leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = true
+
+vim.opt.breakindent = true
+
+vim.opt.shiftwidth = 4
+
+vim.opt.undofile = true
+
+-- searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- quicker update time
+vim.opt.updatetime = 250
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+require("config.lazy")
---
+++ lua/plugins/rose-pine.lua
@@ -0,0 +1,10 @@
+--- rose-pine
+--- https://github.com/rose-pine/neovim
+
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
+}
